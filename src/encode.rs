87548bc62440ac2e85b5938f08a2e8422ca21209
Dan <106247186+DanThePythonMan@users.noreply.github.com>
2024-02-05 21:02:12 +0000

    Update encode.rs

--- src/encode.rs
+++ src/encode.rs
@@ -10,7 +10,7 @@ pub fn encode(text: String, mut key: i32) -> String {
     let char_map: HashMap<String, i32> = HashMap::from([ ("a".to_string(), 1), ("b".to_string(), 2), ("c".to_string(), 3), ("d".to_string(), 4), ("e".to_string(), 5), ("f".to_string(), 6), ("g".to_string(), 7), ("h".to_string(), 8), ("i".to_string(), 9), ("j".to_string(), 10), ("k".to_string(), 11), ("l".to_string(), 12), ("m".to_string(), 13), ("n".to_string(), 14), ("o".to_string(), 15), ("p".to_string(), 16), ("q".to_string(), 17), ("r".to_string(), 18), ("s".to_string(), 19), ("t".to_string(), 20), ("u".to_string(), 21), ("v".to_string(), 22), ("w".to_string(), 23), ("x".to_string(), 24), ("y".to_string(), 25), ("z".to_string(), 26), (" ".to_string(), 0), ]);
 
     let mut encoded_text = String::new();
-    
+
   while key > 26{
       key-=26;
     }
